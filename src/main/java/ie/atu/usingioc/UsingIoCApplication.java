dc1c8cd090abd976f20bfdf67cd2f4a259769283
Gavin <G00402296@atu.ie>
2023-10-10 15:38:06 +0100

    First File Commit

---
+++ src/main/java/ie/atu/usingioc/UsingIoCApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.usingioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingIoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingIoCApplication.class, args);
+    }
+
+}
